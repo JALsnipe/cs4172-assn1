c0037ddff66b7406f07bfe0a62e045eedee2cc7f
Josh Lieberman <josh.lieberman92@gmail.com>
2014-02-11 01:25:24 -0500

    Hae sun, one planet, and satellite.

---
+++ Assets/Scripts/Planet1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet1 : MonoBehaviour {
+
+	public float rotateSpeed;
+	public float orbitSpeed; //degrees
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		var RotateSpeedAlongY = -10.0;
+//		var RotateSpeedAlongZ = 0.0;
+//		var RotateSpeedAlongX = 0.0;
+
+		// Slowly rotate the object around its axis at 1 degree/second * variable.
+//		transform.Rotate(Vector3.up * Time.deltaTime * RotateSpeedAlongY);
+		transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+//		transform.Rotate(Vector3.forward * Time.deltaTime * RotateSpeedAlongZ);
+//		transform.Rotate(Vector3.right * Time.deltaTime * RotateSpeedAlongX);
+
+		// rotate around origin w/ a radius of 2? (Vector3.up)
+		transform.RotateAround(Vector3.zero, Vector3.up, orbitSpeed * Time.deltaTime); //degrees/second
+	
+	}
+}
---
+++ Assets/Scripts/Satellite.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Satellite : MonoBehaviour {
+
+//	public float orbitSpeed;
+	public float degreeSpin;
+	public float speedAroundPlanet;
+	public GameObject origin;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		transform.RotateAround(Vector3.zero, Vector3.up, orbitSpeed * Time.deltaTime); //degrees/second
+
+//		var degrees = 100;
+
+		transform.RotateAround (transform.position, Vector3.up, degreeSpin * Time.deltaTime);
+
+		transform.RotateAround (origin.transform.position, Vector3.down, speedAroundPlanet * Time.deltaTime);
+	
+	}
+}
